134afa98b611b56ff083b551e862e07f39690793
Adam Montgomery <aquinnmo@outlook.com>
2026-08-26 00:23:50 -0400

    vite config update

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/Coop-Reflections/' : '/',
+export default defineConfig(() => ({
+  base: '/',
   plugins: [react()],
 }))
